db1d167a072ea1411925a1056d4547168e6385e1
Spenser Dewey <spenser@dewey.fm>
2022-01-08 16:25:06 -0500

    Add GameObject to client, send and recieve messages

--- client.ts
+++ client.ts
@@ -2,8 +2,10 @@
  * Represents a client; users of polyplay should create a client to connect to a multiplayer room
  */
 class Client {
-    socket: WebSocket;
-    // objects : GameObject[];
+    private socket: WebSocket;
+    private gameObjects : GameObject[];
+    private messageBuffer : JSON[];
+    public currentFrame : number;
 
     /**
      * Connects to a multiplayer room
@@ -12,28 +14,45 @@ class Client {
      * @param address The address of the websocket
      * @returns True if the connection was essential 
      */
-    constructor(roomID: string, clientName: string, address: string) {
+    public constructor(roomID: string, clientName: string, address: string, maxCapacity : number = 0) {
+        this.currentFrame = -1;
         this.socket = new WebSocket(address);
-        // this.objects = [];
-        this.socket.onopen = function (e) {
+        this.gameObjects = [];
+        this.messageBuffer = [];
+        this.socket.onopen = (e) => {
             console.log("Connection established");
-            this.send(JSON.stringify({ roomID, clientName, type: "join" }));
+            this.socket.send(JSON.stringify({ roomID, clientName, maxCapacity, type: "join"}));
 
-            this.onmessage = function (event) {
+            this.socket.onmessage = (event) => {
                 console.log(event.data)
                 let response = JSON.parse(event.data)
-                switch (response.type) {
-                    // TODO: responses
-                    case "update":
-                        break;
-                    case "create":
-                        break;
-                    case "join":
-                        alert("JOIN EVENT ACKNOWLEDGED")
-                        console.log(event.data)
-                        break;
-                    default:
-                        throw new Error("Socket Failed to Connect");
+                try {
+                    switch (response.type) {
+                        case "update":
+                            this.socket.send(JSON.stringify({userInputs: this.messageBuffer}))
+                            this.messageBuffer = []
+                            this.gameObjects.forEach((gameObject : GameObject) => {gameObject.update()});
+                            response.updatedGameObjects.array.forEach((object : any) => {
+                                this.gameObjects[object.id].updateFromData(object.data);
+                            });
+                            break;
+                        case "create":
+                            response.newGameObject.array.forEach((newObject : any) => {
+                                this.gameObjects[newObject.id] = Reflect.construct(newObject.typeOfObject, newObject.paramValues.array);
+                            });
+                            break;
+                        case "join":
+                            console.log('JOINT DATA')
+                            console.log(event.data)
+                            break;
+                        case "error":
+                            console.log(response.type, response.message);
+                        default:
+                            throw new Error("Socket Failed to Connect");
+                    }
+                } catch(error) {
+                    console.error("MALFORMED EVENT");
+                    console.error(event.data);
                 }
             };
         };
@@ -52,7 +71,19 @@ class Client {
         };
     }
 
-    send_event(message : string) : void {
-        this.socket.send(JSON.stringify({message, type: "input"}))
+    /**
+     * Send an event across to the server
+     * @param message The message to send to the server code
+     */
+    public sendEvent(message : JSON) : void {
+        this.messageBuffer.push(message)
+    }
+
+    /**
+     * Get the list of all game objects that are currently active
+     * @returns A list of game objects that have not been destroyed yet
+     */
+    public getGameObjects() : GameObject[] {
+        return this.gameObjects;
     }
 }
--- shared.ts
+++ shared.ts
@@ -1,8 +1,21 @@
 /**
- * Game Objects passed between server and clients; these have a serialized component, 
+ * Game Objects exist on both server and clients; these have a serialized component, 
  *      which is updated each frame by the server
  */
 interface GameObject {
+    /**
+     * Update the game object (on the server, this will be )
+     */
     update() : void;
-    getSerializedData() : JSON;
+    /**
+     * Returns a JSON object of simple fields that represent any non-determinstic data;
+     *  that is to say, data that the clients could not predict based on previous data.
+     *  This is usually caused by another player's input event or game randomness.
+     */
+    getUpdatedData() : JSON;
+    /**
+     *  Update this game object with the same type of JSON data recieved from
+     *   getUpdatedData(); this will be called on the server side to 
+     */
+    updateFromData(inputs: JSON) : void;
 }
